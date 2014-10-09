6b908338e2d73e2bb4b09cec9cf2c39ed5efdc03
Xiahou <admin@iios.net>
2014-10-09 23:53:01 +0800

    u

--- footer.php
+++ footer.php
@@ -1,17 +1,17 @@
-		 
-		<footer class="footer">
-	        <span class="footer__copyright">&copy; 2014  All rights reserved. </span>
-	        
-	        <span class="footer__copyright">友情链接- 
-	        	<a target="_blank" href="http://www.bailong.org/">白龙</a>
-                <a target="_blank" href="http://silentexia.net/">星枫</a>
-                <a target="_blank" href="http://www.byends.com/">羽飞</a>    
-	        </span>
-	        <span class="footer__copyright"> 本站由 
-	        	<a href="http://typecho.org/" target="_blank">Typecho</a> 搭建，基于Ghost主题
-	        	<a href="http://github.com/onevcat/vno" target="_blank">Vno</a> 修改
-	        </span>
-	    </footer>
+<footer class="footer">
+    <span class="footer__copyright">&copy; 2014  All rights reserved. </span>
+    
+    <span class="footer__copyright">友情链接- 
+    	<a target="_blank" href="http://www.bailong.org/">白龙</a>
+        <a target="_blank" href="http://silentexia.net/">星枫</a>
+        <a target="_blank" href="http://www.byends.com/">羽飞</a>    
+    </span>
+    <span class="footer__copyright"> 本站由 
+        <a href="http://typecho.org/" target="_blank">Typecho</a> 搭建，基于
+        <a href="https://ghost.org/" target="_blank">Ghost</a>11主题
+        <a href="http://github.com/onevcat/vno" target="_blank">Vno</a> 修改
+    </span>
+</footer>
 	</div>
 </div>
 
--- page.php
+++ page.php
@@ -1,18 +1,17 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <?php $this->need('header.php'); ?>
 <article class="post-container post-container--single">
-
     <header class="post-header">
-      <div class="post-meta">
-        <time datetime="<?php $this->date(''); ?>" class="post-meta__date date"></time>
-        <span class="post-list__meta--tags tags"><?php $this->category(','); ?></span>
-		<span class="post-list__meta--tags tags"><?php $this->tags('|'); ?></span>
-      </div>
-      <h1 class="post-title"><?php $this->title() ?></h1>
+        <div class="post-meta">
+            <time datetime="<?php $this->date(''); ?>" class="post-meta__date date"></time>
+            <span class="post-list__meta--tags tags"><?php $this->category(','); ?></span>
+		  <span class="post-list__meta--tags tags"><?php $this->tags('|'); ?></span>
+        </div>
+        <h1 class="post-title"><?php $this->title() ?></h1>
     </header>
 
     <section class="post">
-      <p><?php $this->content(); ?></p>
+        <p><?php $this->content(); ?></p>
     </section>
 
   </article>
