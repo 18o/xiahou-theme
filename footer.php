8d231024ec466ca3a0287d3b85ff4c359c93036e
XiaHou <admin@iios.net>
2016-02-03 03:45:30 +0800

    u

--- footer.php
+++ footer.php
@@ -1,22 +1,22 @@
 <footer id="footer" class="footer">
     <div class="footer-inner">
         <div class="copyright" >
-            &copy;  2011 -
-            <span itemprop="copyrightYear">2016</span>
+            &copy;  2013 -
+            <span>2016</span>
           <span class="with-love">
             <i class="icon-next-heart fa fa-heart"></i>
           </span>
-            <span class="author" itemprop="copyrightHolder">IIssNan</span>
+            <span class="author">夏侯</span>
         </div>
 
         <div class="powered-by">
-            由 <a class="theme-link" href="http://hexo.io">Hexo</a> 强力驱动
+            由 <a class="theme-link" href="http://typecho.org/">Tyecho</a> 强力驱动
         </div>
 
         <div class="theme-info">
             主题 -
-            <a class="theme-link" href="https://github.com/iissnan/hexo-theme-next">
-                NexT.Pisces
+            <a class="theme-link" href="https://git.coding.net/administrater/Xiahou-Theme.git">
+                NexT.Xiahou
             </a>
         </div>
 
