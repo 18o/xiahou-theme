c1a681fa1c7437cc0936cfdc9a36eecc24b3c61e
Tree <admin@iios.net>
2016-02-04 22:33:40 +0800

    u

--- pagination.php
+++ pagination.php
@@ -9,42 +9,22 @@ $total_page = $this->getTotalPage();
 
 <nav class="pagination">
 
+    <a class="extend prev" rel="prev" href="/page/<?php echo $current_page - 1 > 0 ?: 1; ?>">
+        <i class="fa fa-angle-left"></i>
+    </a>
+    <?php
+    $start = $end = $current_page;
 
-    <?php if($current_page == 1) {?>
-        <span class="page-number current"><?php echo $this->getCurrentPage();?></span>
-
-    <?php } elseif($current_page == 2) {?>
-        <a class="extend prev" rel="prev" href="/">
-            <i class="fa fa-angle-left"></i>
-        </a>
-    <?php } else  { ?>
-        <a class="extend prev" rel="prev" href="/page/<?php echo $this->getCurrentPage() - 1; ?>">
-            <i class="fa fa-angle-left"></i>
+    for($i = $current_page - 2; $i < $current_page + 2; $i++) {
+    if($i < 1 || $i > $total_page) continue;
+    ?>
+        <a class="extend prev" rel="prev" href="/page/<?php echo $i; ?>">
+            <?php echo $i; ?>
         </a>
     <?php }?>
 
-    <?php for($i = $current_page; $i < ($current_page + 5 < $current_page ?: $current_page); $i++) {
-        echo $i . "--";
-    }?>
-
-
-
-    <a class="page-number" href="/">1</a>
-    <span class="page-number current">2</span>
-    <a class="page-number" href="/page/3/">3</a>
-    <span class="space">&hellip;</span>
-    <a class="page-number" href="/page/37/">37</a>
-    <a class="extend next" rel="next" href="/page/<?php echo $this->getTotalPage();?>/">
+    <a class="extend next" rel="next" href="/page/<?php echo $total_page;?>/">
         <i class="fa fa-angle-right"></i>
     </a>
+
 </nav>
-
-
-
-<nav class="pagination" role="navigation">
-    <?php $this->pageLink('<span class="newer-posts pagination__newer btn btn-small btn-tertiary">&larr; 最近</span>'); ?>
-    <span class="pagination__page-number"><?php echo $this->getCurrentPage() ?> / <?php echo $this->getTotalPage() ?></span>
-    <?php $this->pageLink('<span class="older-posts pagination__older btn btn-small btn-tertiary">更早 &rarr;</span>', 'next'); ?>
-</nav>
-<?php $this->pageLink('下一页','next'); ?>
-<?php $this->pageLink('上一页'); ?>
