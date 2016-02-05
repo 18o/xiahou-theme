1bdc36adbb55afbb8227dda7dadfb677b74cfe2f
CuiZheng <zheng.cui@salonwith.com>
2016-02-05 11:13:21 +0800

    修改分页代码

--- pagination.php
+++ pagination.php
@@ -6,41 +6,37 @@ $total_page = $this->getTotalPage();
 
 
 <?php
-//$this->pageNav(
-//    '<i class="fa fa-angle-left"></i>',
-//    '<i class="fa fa-angle-right"></i>',
-//    3,
-//    '...',
-//    [
-//        'wrapTag' => 'nav',
-//        'wrapClass' => 'pagination',
-//        'itemTag' => 'span',
-//        'textTag' => 'span',
-//        'currentClass' => 'current',
-//        'prevClass' => 'extend',
-//        'nextClass' => 'extend'
-//
-//    ]);
+$this->pageNav(
+   '<i class="fa fa-angle-left"></i>',
+   '<i class="fa fa-angle-right"></i>',
+   3,
+   '...',
+   [
+       'wrapTag' => 'nav',
+       'wrapClass' => 'pagination',
+       'itemTag' => 'span',
+       'textTag' => 'span',
+       'currentClass' => 'current',
+       'prevClass' => 'extend',
+       'nextClass' => 'extend'
+
+   ]);
 ?>
 
-<nav class="pagination">
 
-    <a class="extend prev" rel="prev" href="/page/<?php echo $current_page - 1 > 0 ?: 1; ?>">
-        <i class="fa fa-angle-left"></i>
-    </a>
-    <?php
-    $start = $end = $current_page;
-
-    for($i = $current_page - 2; $i < $current_page + 2; $i++) {
-    if($i < 1 || $i > $total_page) continue;
-    ?>
-        <a class="extend prev" rel="prev" href="/page/<?php echo $i; ?>">
-            <?php echo $i; ?>
-        </a>
-    <?php }?>
-
-    <a class="extend next" rel="next" href="/page/<?php echo $total_page;?>/">
-        <i class="fa fa-angle-right"></i>
-    </a>
-
-</nav>
+<style type="text/css">
+    .pagination span {
+        display: inline-block;
+        width: 30px;
+        height: 30px;
+        cursor: pointer;
+        border-top: 1px solid #eee;
+        margin-top: -1px;
+    }
+    .pagination span a {
+        border-bottom: none;
+    }
+    .pagination span:hover {
+        border-top: 1px solid black;
+    }
+</style>
