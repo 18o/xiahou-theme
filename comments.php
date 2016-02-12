c4c47037f75f7f40b9f537c82b67204cca7874aa
Tree <admin@iios.net>
2016-02-12 09:30:57 +0800

    修改评论html

--- comments.php
+++ comments.php
@@ -18,33 +18,26 @@
             </div>
             <span id="response" class="widget-title"><?php _e('添加新评论');?></span>
             <form method="post" action="<?php $this->commentUrl()?>" id="comment-form">
-                <div>
-                    <?php if ($this->user->hasLogin()): ?>
-                        <p><?php _e('登录身份：');?><a href="<?php $this->options->profileUrl();?>"><?php $this->user->screenName();?></a>. <a href="<?php $this->options->logoutUrl();?>" title="Logout"><?php _e('退出');?> &raquo;</a></p>
-                    <?php else: ?>
-                        <p class="comment-about">
-                            <label for="author" class="required"><?php _e('称呼');?></label>
-                            <input type="text" name="author" id="author" class="text" value="<?php $this->remember('author');?>"/>
-                        </p>
-                        <p class="comment-about">
-                            <label for="mail"<?php if ($this->options->commentsRequireMail): ?> class="required"<?php endif;?>><?php _e('邮箱');?></label>
-                            <input type="email" name="mail" id="mail" class="text" value="<?php $this->remember('mail');?>"/>
-                        </p>
-                        <p class="comment-about">
-                            <label for="url"<?php if ($this->options->commentsRequireURL): ?> class="required"<?php endif;?>><?php _e('网站');?></label>
-                            <input type="url" name="url" id="url" class="text" placeholder="<?php _e('http://example.com');?>" value="<?php $this->remember('url');?>"/>
-                        </p>
-                    <?php endif;?>
-                    <p>
-                        <textarea rows="8" cols="50" name="text" class="text-area"><?php $this->remember('text');?></textarea>
+                <?php if ($this->user->hasLogin()): ?>
+                    <p><?php _e('登录身份：');?><a href="<?php $this->options->profileUrl();?>"><?php $this->user->screenName();?></a>. <a href="<?php $this->options->logoutUrl();?>" title="Logout"><?php _e('退出');?> &raquo;</a></p>
+                <?php else: ?>
+                    <p class="comment-about">
+                        <label for="author" class="required"><?php _e('称呼');?></label>
+                        <input type="text" name="author" id="author" class="text" value="<?php $this->remember('author');?>"/>
                     </p>
-                </div>
+                    <p class="comment-about">
+                        <label for="mail"<?php if ($this->options->commentsRequireMail): ?> class="required"<?php endif;?>><?php _e('邮箱');?></label>
+                        <input type="email" name="mail" id="mail" class="text" value="<?php $this->remember('mail');?>"/>
+                    </p>
+                    <p class="comment-about">
+                        <label for="url"<?php if ($this->options->commentsRequireURL): ?> class="required"<?php endif;?>><?php _e('网站');?></label>
+                        <input type="url" name="url" id="url" class="text" placeholder="<?php _e('http://example.com');?>" value="<?php $this->remember('url');?>"/>
+                    </p>
+                <?php endif;?>
+                <textarea rows="8" cols="50" name="text" class="text-area"><?php $this->remember('text');?></textarea>
                 <div class="col2">
-                    <p>
-                        <button type="submit" class="submit"><?php _e('提交评论');?></button>
-                    </p>
+                    <button type="submit" class="submit"><?php _e('提交评论');?></button>
                 </div>
-                <div class="clear"></div>
             </form>
         </div>
     <?php else: ?>
